7c16dcc7c40816be43d7c1525340b584f0815648
haldric <lebarmic@hotmail.com>
2012-12-21 17:27:38 +0100

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Thu Dec 20 16:46:20 CET 2012"
+#define LINUX_COMPILE_BY "haldric"
+#define LINUX_COMPILE_HOST "haldric-P4M266A-8235"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
